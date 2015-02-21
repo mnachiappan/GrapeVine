c741781dbb3b0e8eed1a71ca5a81f3df0de5ef1b
Meyyappan Nachiappan <mnachiappan1995@gmail.com>
2015-02-21 16:16:26 -0500

    list view created

---
+++ app/src/main/java/com/mnachiappan/graphmessage/ConversationFragment.java
@@ -0,0 +1,41 @@
+package com.mnachiappan.graphmessage;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ConversationFragment extends Fragment {
+
+    private ArrayAdapter<String> mAdapter;
+
+    public ConversationFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] conversations = new String[] {"Hello", "Bye"};
+        List<String> conversationsList = new ArrayList<String>(Arrays.asList(conversations));
+
+        // create ListView
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_conversation);
+        // create adapter
+        mAdapter = new ArrayAdapter<String>(this.getActivity(), R.layout.list_item_conversation, R.id.list_item_conversation_textview, conversationsList);
+        // hook adapter with ListView
+        listView.setAdapter(mAdapter);
+        return rootView;
+    }
+}
